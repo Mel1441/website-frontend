12e9a8e60a7abe137eded244be31453bad5a5477
Mel1441 <masonlm68@gmail.com>
2025-10-08 11:57:51 -0400

    put fe back in its own folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom';
+import avatar from '../assets/IMG_3974_v1.jpeg';
 
 export default function HomePage() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Welcome to My Portfolio</h1>
+      <body className="min-h-screen bg-[#FFF0F0] text-gray-700">
 
-      <p className="mb-6">
-        I build secure, modular, and audit-friendly systems. Here's a preview of a login flow I designed:
-      </p>
+      <div className=" grid grid-cols-4">
+ 
+        <div className="col-span-1"/>
+        <img src={avatar} alt="Melissa Avatar" className="w-100 h-100 object-contain mt-25 ml-25 col-span-1 shadow-sm"/>
 
+
+        <div className="col-span-1">
+        <h1 className="">
+          Senior Data Engineer!!!
+        </h1>
+        <p className="">
+          Under Construction
+        </p>
+        <div className="col-span-1"/>
+</div>
+      </div>
+
+    <div className="container mx-auto flex  lg:flex-row p-10">
+      <h1 className="flex-col text-3xl font-bold mr-10">Projects</h1>
+    {/* <div className='container mx-auto flex flex-col sm:flex-row p-8'> */}
+    <h1 className='flex-col text-3xl font-bold'>HELLO</h1></div>
+    <div className='container mx-auto flex flex-col sm:flex-row p-8'>
       <Link
         to="/school/login"
         className="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
@@ -16,5 +34,8 @@ export default function HomePage() {
         View Login Demo
       </Link>
     </div>
+    <div className='p-8'>
+    </div>
+    </body>
   );
 }
